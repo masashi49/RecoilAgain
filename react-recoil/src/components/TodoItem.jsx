1217228310e11a23d2708d6bc6ed614578d17ef3
yamadamasashi <shisama94@gmail.com>
2023-02-28 09:45:04 +0900

    filterとsliceの時間no

--- react-recoil/src/components/TodoItem.jsx
+++ react-recoil/src/components/TodoItem.jsx
@@ -6,18 +6,33 @@ const TodoItem = ({ id, title }) => {
   const [todoList, setTodoList] = useRecoilState(todoListState);
 
   const deleteItem = () => {
-    const index = todoList.findIndex((item) => item.id === id);
-    const newTodoList = [
-      ...todoList.slice(0, index),
-      ...todoList.slice(index + 1), //[1,2,3,4,5,6,7] -> [1,2,3]と[5,6,7]をつなぐことで取り除く
-    ];
-    setTodoList(newTodoList);
+    console.time("timer1");
+    for (let index = 0; index < 10; index++) {
+      const newState = todoList.filter((item) => item.id !== id);
+      setTodoList(newState);
+    }
+    console.timeEnd("timer1");
+  };
+
+  const deleteItem2 = () => {
+    console.time("timer1");
+    for (let index = 0; index < 10; index++) {
+      const index = todoList.findIndex((item) => item.id === id);
+      //[1,2,3,4,5,6,7] ->  [1,2,3]と[5,6,7]を繋ぐことで4を取り除く
+      const newTodoList = [
+        ...todoList.slice(0, index),
+        ...todoList.slice(index + 1),
+      ];
+      setTodoList(newTodoList);
+    }
+    console.timeEnd("timer1");
   };
 
   return (
     <div key={id}>
       {id}:{title}
-      <button onClick={deleteItem}>削除</button>
+      <button onClick={deleteItem}>削除 deleteItem</button>
+      <button onClick={deleteItem2}>削除 deleteItem2</button>
     </div>
   );
 };
